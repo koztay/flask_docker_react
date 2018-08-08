fbce5c4786fe3662c3a2c0a2650589508a1ea637
koztay <koztay@me.com>
2018-08-08 15:52:33 +0300

    postgres eklendi

---
+++ services/users/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+echo "Waiting for postgres..."
+
+
+while ! nc -z users-db 5432; do
+    sleep 0.1
+done
+
+
+echo "PostgreSQL stareted"
+
+python manage.py run -h 0.0.0.0
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,22 +1,27 @@
 # services/users/project/config.py
 
 
+import os
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODOFOCATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Testing Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 

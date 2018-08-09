9fd6ec551f75ac85daa113b1b2cf5f240e3f31b2
koztay <koztay@me.com>
2018-08-09 18:49:52 +0300

    production server, nginx eklendi

---
+++ services/users/entrypoint-prod.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+echo "Waiting for postgres..."
+
+
+while ! nc -z users-db 5432; do
+    sleep 0.1
+done
+
+
+echo "PostgreSQL started"
+
+gunicorn -b 0.0.0.0:5000 manage:app
+
+

71c5b56de0245b6e71aebce6a8f6f660f9e1b924
koztay <koztay@me.com>
2018-08-12 21:00:09 +0300

    react test konfigürasyonu yapıldı,bazı testler yazıldı ve çalıştırıldı

---
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+
+
+import UsersList from '../UsersList';
+
+
+const users = [
+    {
+        'active': true,
+        'email': 'kemal@abcd.com',
+        'id': 1,
+        'username': 'kemal'
+    },
+    {
+        'active': true,
+        'email': 'sevval@abcd.com',
+        'id': 2,
+        'username': 'sevval'
+    }
+];
+
+
+test('UsersList renders properly', () => {
+   const wrapper = shallow(<UsersList users={users} />);
+   const element = wrapper.find('h4');
+   expect(element.length).toBe(2);
+   expect(element.get(0).props.className).toBe('card card-body bg-light');
+   expect(element.get(0).props.children).toBe('kemal');
+});
+
+test('UsersList renders a snapshot properly', () => {
+    const tree = renderer.create(<UsersList users={users}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+});

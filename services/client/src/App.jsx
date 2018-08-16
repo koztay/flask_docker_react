a4448499007123c090dc216b80273ea0a9ff4296
koztay <koztay@me.com>
2018-08-16 19:26:55 +0300

    login logout formu eklendi

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 import UserList from './components/UsersList'
 import AddUser from './components/AddUser'
 import About from './components/About'
+import NavBar from './components/NavBar'
+import Form from "./components/Form";
 
 
 class App extends Component {
@@ -16,6 +18,12 @@ class App extends Component {
             users: [],
             username: '',
             email: '',
+            title: 'TestDriven.io',
+            formData: {
+                username: '',
+                email: '',
+                password: ''
+            }
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -61,29 +69,42 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <br/>
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr/>
-                                    <br/>
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        handleChange={this.handleChange}
-                                        addUser={this.addUser}/>
-                                    <br/>
-                                    <UserList users={this.state.users}/>
-                                </div>
-                            )}/>
-                            <Route exact path='/about' component={About}/>
-                        </Switch>
-                        <br/>
-                        <About/>
+            <div>
+                <NavBar title={this.state.title}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr/>
+                                        <br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            handleChange={this.handleChange}
+                                            addUser={this.addUser}/>
+                                        <br/>
+                                        <UserList users={this.state.users}/>
+                                    </div>
+                                )}/>
+                                <Route exact path='/about' component={About}/>
+                                <Route exact path='/register' render={() => (
+                                    <Form
+                                        formType={'Register'}
+                                        formData={this.state.formData}
+                                    />
+                                )}/>
+                                <Route exact path='/login' render={() => (
+                                    <Form
+                                        formType={'Login'}
+                                        formData={this.state.formData}
+                                    />
+                                )}/>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -7,7 +7,7 @@ const UserList = (props) => {
           {
             props.users.map((user) => {
                 return (
-                    <h4 key={user.id} className="card card-body bg-light">
+                    <h4 key={user.id} className="well">
                       {user.username}
                     </h4>
                 )
